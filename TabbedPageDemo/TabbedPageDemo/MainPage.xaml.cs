947bb62449a76f8e531fcfd96d385ac0b4e82f51
InfoBrother <Infobrother33@gmail.com>
2018-02-16 11:27:44 +0400

    Initial commit

---
+++ TabbedPageDemo/TabbedPageDemo/MainPage.xaml.cs
@@ -0,0 +1,16 @@
+﻿using Xamarin.Forms;
+
+namespace TabbedPageDemo
+{
+	public partial class MainPage : TabbedPage
+    {
+		public MainPage()
+		{
+			InitializeComponent();
+
+            Children.Add(new Cplusplus());
+            Children.Add(new csharp());
+            Children.Add(new xamarin());
+		}
+	}
+}
---
+++ TabbedPageDemo/TabbedPageDemo/xamarin.xaml.cs
@@ -0,0 +1,13 @@
+﻿using Xamarin.Forms;
+
+namespace TabbedPageDemo
+{
+	
+	public partial class xamarin : ContentPage
+	{
+		public xamarin ()
+		{
+			InitializeComponent ();
+		}
+    }
+}
